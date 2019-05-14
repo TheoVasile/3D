24d770331b412e8c04fd354869f1f6ca94b69c2f
Theo Vasile <theo.vasile50@yahoo.com>
2019-05-14 14:58:20 -0400

    completed point of intersection equation for line and plane, and added line and plane class

--- D3/calculate.py
+++ D3/calculate.py
@@ -20,15 +20,27 @@ class Line(Equation):
         self.c = c
     def get_direction(self):
         return self.a, self.b, self.c
-    def get_pos0(self):
-        return self.x, self.y, self.z
     def calculate(self, t):
         x = self.x + t * self.a
         y = self.y + t * self.b
         z = self.z + t * self.c
         return x, y, z
 
-
+class Plane(Equation):
+    def __init__(self, a, b, c, d):
+        super().__init__(a, b, c)
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+    def get_normal(self):
+        return self.a, self.b, self.c
+    def set_normal(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+    def set_displace(self, d):
+        self.d = d
 
 class Calculate:
     def dist(self, point1, point2):
@@ -41,9 +53,17 @@ class Calculate:
         c = (point2[2] - point1[2]) / t
 
         return point1, (a, b, c)
-    def POI(self, ob1, ob2):
+    def POI(self, ob1, ob2, ob3 = None):
         #point of intersection between line and plane
         if (type(ob1) or type(ob2)) == 'Line' and (type(ob1) or type(ob2)) == 'Plane':
             plane = [ob1, ob2][[type(ob1), type(ob2)].index('Plane')]
             line  = [ob1, ob2][[type(ob1), type(ob2)].index('Line')]
 
+            t = plane.get_normal()[0] * line.get_direction()[0] + plane.get_normal()[1] * line.get_direction()[1] + plane.get_normal()[2] * line.get_direction()[2]
+            t = plane.get_displace() / t
+
+            point = line.calculate(t)
+        elif (type(ob1) and type(ob2) and type(ob3)) == 'Plane':
+            pass
+        elif (type(ob1) and type(ob2)) == 'Line':
+            pass
